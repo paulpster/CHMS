675b75b557afec11673b3cba85b263e57c917a91
Paul Power <paul.power@solutionsbyblueprint.com>
2024-02-14 16:09:42 -0700

    YAY! they install via the command line now.

--- SQL/daily_report.sql
+++ SQL/daily_report.sql
@@ -2,15 +2,16 @@
 USE chms;
 DROP PROCEDURE IF EXISTS chms.daily_report;
 
-DELIMETER $$
-CREATE PROCEDURE chms.daily_report(IN report_date date)
+DELIMITER ;;
+
+CREATE PROCEDURE `daily_report`(IN report_date date)
 BEGIN
     SELECT c.customer_name, v.vehicle_name, b.date_out, b.date_in 
         FROM chms.booking b INNER JOIN chms.customer c ON b.customer_id = c.customer_id
             INNER JOIN chms.vehicle v ON b.vehicle_id = v.vehicle_id
         WHERE _report_date BETWEEN b.date_out AND b.date_in ORDER BY b.date_out, c.customer_name, v.vehicle_name;
-END$$
+END ;;
 
-DELIMETER ;
+DELIMITER ;
 
 
