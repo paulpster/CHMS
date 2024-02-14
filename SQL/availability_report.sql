07016d0fca4c6c0dc9cde35210447ff31443543d
Paul Power <paul.power@solutionsbyblueprint.com>
2024-02-14 15:05:03 -0700

    an attempt for an availabililty report

---
+++ SQL/availability_report.sql
@@ -0,0 +1,12 @@
+USE chms;
+DROP PROCEDURE IF EXISTS chms.availability_report;
+
+DELIMETER $$
+CREATE OR REPLACE PROCEDURE chms.availability_report(IN _report_date date)
+BEGIN
+    SELECT v.vehicle_id, v.vin, v.vehicle_name, v.vehicle_type 
+    FROM chms.vehicle v INNER JOIN chms.booking b ON (v.vehicle_id = b.vehicle_id AND _report_date BETWEEN b.date_out AND b.date_in)
+    WHERE b.vehicle_id IS NULL ORDER BY v.vehicle_name;
+END$$
+
+DELIMETER ;
