6deae3fa99955cb70381015ec4da4f6323e1c08d
Paul Power <paul.power@solutionsbyblueprint.com>
2024-02-14 19:39:18 -0700

    first tests for DB connection

---
+++ tests/test_mysql_conn.py
@@ -0,0 +1,44 @@
+import pytest
+
+from chms.utils.mysql_conn import MySQLConn
+from chms.utils.commons import DB_DB, DB_HOST, DB_PWD, DB_USER
+
+
+def test_make_connection():
+    DB = MySQLConn(database=DB_DB, user=DB_USER, password=DB_PWD, host=DB_HOST)
+
+    assert DB is not None
+    return
+
+
+def test_store():
+    DB = MySQLConn(database=DB_DB, user=DB_USER, password=DB_PWD, host=DB_HOST)
+
+    assert DB is not None
+
+    # TODO: things are still cockeyed. this record is not in the DB
+    # I have a connection/commit/config issue
+    SQL = "INSERT INTO chms.customer (customer_name, customer_address, contact_number) VALUES (%s, %s, %s);"
+    DB.store(SQL, ("Paul Power", "Billings, MT", "4065551212"))
+    return
+
+
+def test_execute():
+    DB = MySQLConn(database=DB_DB, user=DB_USER, password=DB_PWD, host=DB_HOST)
+
+    assert DB is not None
+
+    rs = DB.query("SELECT * FROM chms.customer ORDER BY customer_id", ())
+    assert rs is not None
+    return
+
+
+def test_callproc():
+    DB = MySQLConn(database=DB_DB, user=DB_USER, password=DB_PWD, host=DB_HOST)
+
+    assert DB is not None
+
+    sql = "chms.daily_report"
+    rs = DB.callProc(sql, ("2024-02-14",))
+    assert rs is not None
+    return
