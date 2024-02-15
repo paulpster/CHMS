fa122c31311d48daae00cc4484cc030393f7fcfb
Paul Power <paul.power@solutionsbyblueprint.com>
2024-02-14 19:10:31 -0700

    fixed syntax error arount the report date

--- SQL/daily_report.sql
+++ SQL/daily_report.sql
@@ -4,12 +4,12 @@ DROP PROCEDURE IF EXISTS chms.daily_report;
 
 DELIMITER ;;
 
-CREATE PROCEDURE `daily_report`(IN report_date date)
+CREATE PROCEDURE `daily_report`(IN _report_date date)
 BEGIN
     SELECT c.customer_name, v.vehicle_name, b.date_out, b.date_in 
         FROM chms.booking b INNER JOIN chms.customer c ON b.customer_id = c.customer_id
             INNER JOIN chms.vehicle v ON b.vehicle_id = v.vehicle_id
-        WHERE _report_date BETWEEN b.date_out AND b.date_in ORDER BY b.date_out, c.customer_name, v.vehicle_name;
+        WHERE b.date_out < _report_date AND b.date_in > _report_date ORDER BY b.date_out, c.customer_name, v.vehicle_name;
 END ;;
 
 DELIMITER ;
