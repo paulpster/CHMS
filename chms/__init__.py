625bca16cf7cb89502a4c4e32aea3f67f19d9b0d
Paul Power <paul.power@solutionsbyblueprint.com>
2024-02-14 19:10:59 -0700

    eliminated some things i do not need.

--- chms/__init__.py
+++ chms/__init__.py
@@ -1,22 +1,18 @@
 import os
 
 from flask import Flask
-from werkzeug.middleware.proxy_fix import ProxyFix
 
 
 def create_app(test_config=None):
     # create and configure app
     app = Flask(__name__, instance_relative_config=True)
 
-    try:
-        fh = open("location.dat")
-        print("found location.dat :)")
-        loc = fh.readline().strip()
-    except:
-        print("location.dat not found.")
-        loc = ""
+    app.config.from_mapping(SECRET_KEY="KLJhKJHkljJH")
 
-    app.config.from_mapping(SECRET_KEY="KLJhKJHkljJH", APP_ROOT=loc)
+    # from utils.utils import teardown_req, before_req
+
+    # app.teardown_appcontext(teardown_req)
+    # before_req()
 
     if test_config is None:
         app.config.from_pyfile("config.py", silent=True)
@@ -27,8 +23,6 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-
-    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
     @app.route("/hello")
     def hello():
